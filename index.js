564887ba08db957bfbe8e5ef697786ac0a4ebb84
Aaron Lin <aaron.l.lin.general@gmail.com>
2024-04-02 15:31:40 -0400

    1

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(a = 'roller-skate') {
+    let defaultWords = `This Saturday, I want to ${a}!`
+    return defaultWords
+}
+function mondayWork(a= 'go to the office') {
+    let defaultWords = `This Monday, I will ${a}.`
+    return defaultWords
+}
+function wrapAdjective(flair) {
+    return function doIt(a='special'){
+        return `You are ${flair}${a}${flair}!`
+    }
+}
